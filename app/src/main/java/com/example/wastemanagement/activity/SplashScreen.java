4661166da2769be50713ddb3fb9ef68a81099fb7
farhan aldiansyah <farhanpoetra011@gmail.com>
2021-11-01 22:29:55 +0700

    splash screen

---
+++ app/src/main/java/com/example/wastemanagement/activity/SplashScreen.java
@@ -0,0 +1,16 @@
+package com.example.wastemanagement.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.wastemanagement.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
